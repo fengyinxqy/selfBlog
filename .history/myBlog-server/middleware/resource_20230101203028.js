11b16682953ce9502c6d95ff5183bb26d68ced4e
Petrichor <572752189@qq.com>
2023-01-18 20:00:01 +0800

    删除了一些文件

--- .history/myBlog-server/middleware/resource_20230101203028.js
+++
@@ -1,9 +0,0 @@
-const { classify } = require('inflection')
-
-module.exports = options => {
-  return async (req, res, next) => {
-    const modelName = classify(req.params.resource);
-    req.Model = require(`../models/${modelName}`)
-    next()
-  }
-}
